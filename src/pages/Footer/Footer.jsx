8cd6cd1cb97b0ec28eb4f0a38b9e9b63d425ae68
zhenyue-555 <amanda.yuzy@gmail.com>
2023-11-15 20:01:27 -0500

    adjust @media, add footer links

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -10,17 +10,17 @@ export const Footer = () => {
       </div>
       <ul className="footer--links">
         <li className="footer--link">
-        <a href="mailto:myemail@email.com">
+        <a href="mailto:amanda.yuzy@gmail.com">
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />Gmail</a>
         </li>
         <li className="footer--link">
-        <a href="https://www.linkedin.com/myname">
+        <a href="https://www.linkedin.com/in/zhenyue-yu-2062a11a9/">
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"/>Linkedin</a>
         </li>
         <li className="footer--link">
-        <a href="https://www.github.com/myname">
+        <a href="https://github.com/zhenyue-555">
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />GitHub</a>
         </li>
       </ul>
