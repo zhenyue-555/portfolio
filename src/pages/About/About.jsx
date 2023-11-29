3f227ad58647084b1a49d942d7013ec63caa4828
zhenyue-555 <amanda.yuzy@gmail.com>
2023-11-29 02:18:26 -0500

    use map loop for about.jsx and move about data to moment.json

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,51 +2,37 @@ import React from "react";
 import "../scss/About.scss";
 import { getImageUrl } from "../../utils";
 import { Container, Row, Col, Image } from 'react-bootstrap';
+import {aboutItems} from "../../data/moment.json";
 
 export const About = () => {
+
   return (
-      <Container className="about--section" id="about">
-        <h2 className="about--title">About</h2>
-        <Row className="align-items-center about--content">
-          <Col lg={5} className="d-none d-lg-block">
-            <Image
-              src={getImageUrl("about/aboutImage.png")}
-              alt="Me sitting with a laptop"
-              className="aboutImage"
-            />
-          </Col>
-          <Col lg={7}>
-            <ul className="aboutItems">
-              <li className="aboutItem">
+    <Container className="about--section" id="about">
+      <h2 className="about--title">About</h2>
+      <Row className="align-items-center about--content">
+        <Col lg={5} className="d-none d-lg-block">
+          <Image
+            src={getImageUrl("about/aboutImage.png")}
+            alt="Me sitting with a laptop"
+            className="aboutImage"
+          />
+        </Col>
+        <Col lg={7}>
+          <ul className="aboutItems">
+            {aboutItems.map((item, index) => (
+              <li className="aboutItem" key={index}>
                 <div className="d-none d-md-block d-lg-block">
-                <Image src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-                </div>
-                <div>
-                  <h3 className="title">Front End Skills</h3>
-                  <p className="description">HTML/CSS, React, JavaScript, Responsive Web Design</p>
-                </div>
-              </li>
-              <li className="aboutItem">
-              <div className="d-none d-md-block d-lg-block">
-                <Image src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+                  <Image src={getImageUrl(`about/${item.icon}`)} alt={`${item.title} icon`} />
                 </div>
                 <div className="aboutItemText">
-                  <h3 className="title">Back End Skills</h3>
-                  <p className="description">Node.js, MySQL, MongoDB, API development, Docker, AWS</p>
+                  <h3 className="title">{item.title}</h3>
+                  <p className="description">{item.description}</p>
                 </div>
               </li>
-              <li className="aboutItem">
-              <div className="d-none d-md-block d-lg-block">
-                <Image src={getImageUrl("about/uiIcon.png")} alt="UI Icon" />
-                </div>
-                <div className= "aboutItemText">
-                  <h3 className="title">UI Designer</h3>
-                  <p className="description">Adobe Creative Suite, Figma</p>
-                </div>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   );
 };
