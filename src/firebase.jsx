a21623e4799496f8eb20ae631420a7853274878f
zhenyue-555 <amanda.yuzy@gmail.com>
2023-11-29 01:57:57 -0500

    add comment and subpoint for todo

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,16 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    databaseURL: "https://<your-database-name>.firebaseio.com",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id",
-    measurementId: "your-measurement-id"
-};
-
+    ...
+  };
 
 const app = initializeApp(firebaseConfig);
 
--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,17 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { database } from '../../firebase';
 import { ref, push, update, remove, onValue } from 'firebase/database';
-import { Container, Row, Col, Form, Button, ListGroup, InputGroup } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, ListGroup, InputGroup, Collapse, ListGroupItem } from 'react-bootstrap';
 import '../scss/Todo.scss';
 
 function Todo() {
-    const [text, setText] = useState("");
+    const [taskText, setTaskText] = useState("");
+    const [subtaskText, setSubtaskText] = useState("");
     const [tasks, setTasks] = useState([]);
+    const [openComments, setOpenComments] = useState({});
+    const [openSubtasks, setOpenSubtasks] = useState({});
 
-
-    const changeText = (e) => {
-        setText(e.target.value);
+    const toggleComment = (id) => {
+        setOpenComments(prevOpenComments => ({
+            ...prevOpenComments,
+            [id]: !prevOpenComments[id]
+        }));
     };
+
+    const toggleSubtaskVisibility = (taskId) => {
+        setOpenSubtasks(prevOpenSubtasks => ({
+            ...prevOpenSubtasks,
+            [taskId]: !prevOpenSubtasks[taskId]
+        }));
+    };
+
+    const addSubtask = (taskId, subtaskText) => {
+        const newSubtask = { text: subtaskText, completed: false }; // Create a subtask object
+        const updatedTasks = tasks.map(task => {
+            if (task.id === taskId) {
+                const newSubtasks = task.subtasks ? [...task.subtasks, newSubtask] : [newSubtask];
+                return { ...task, subtasks: newSubtasks };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+        
+        const taskToUpdate = tasks.find(task => task.id === taskId);
+        update(ref(database, `todos/${taskId}`), { ...taskToUpdate, subtasks: taskToUpdate.subtasks ? [...taskToUpdate.subtasks, newSubtask] : [newSubtask] });
+        setSubtaskText("");
+    };
+
+
+    
+    const handleCommentChange = (id, newComment) => {
+        const updatedTasks = tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, comment: newComment };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+        update(ref(database, `todos/${id}`), { comment: newComment });
+    };
+
+
+    // const changeText = (e) => {
+    //     setText(e.target.value);
+    // };
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -25,8 +71,41 @@ function Todo() {
         update(ref(database, `todos/${id}`), { completed: !task.completed });
     };
     
+    const toggleSubtaskCompletion = (taskId, subtaskIndex) => {
+        const updatedTasks = tasks.map(task => {
+            if (task.id === taskId) {
+                const newSubtasks = task.subtasks.map((subtask, index) => {
+                    if (index === subtaskIndex) {
+                        return { ...subtask, completed: !subtask.completed };
+                    }
+                    return subtask;
+                });
+                return { ...task, subtasks: newSubtasks };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+
+        const taskToUpdate = updatedTasks.find(task => task.id === taskId);
+        update(ref(database, `todos/${taskId}`), { ...taskToUpdate });
+    };
+    
     const removeTask = (id) => {
         remove(ref(database, `todos/${id}`));
+    };
+
+    const deleteSubtask = (taskId, subtaskIndex) => {
+        const updatedTasks = tasks.map(task => {
+            if (task.id === taskId) {
+                const newSubtasks = task.subtasks.filter((_, index) => index !== subtaskIndex);
+                return { ...task, subtasks: newSubtasks };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+
+        const taskToUpdate = updatedTasks.find(task => task.id === taskId);
+        update(ref(database, `todos/${taskId}`), { ...taskToUpdate });
     };
     
     useEffect(() => {
@@ -47,13 +126,13 @@ function Todo() {
             <Row className="todo-row justify-content-center align-items-center">
                 <Col xs={12} md={8} className="shadow p-3 bg-white">
                     <h5 className="text-center text-primary">Todo List</h5>
-                    <Form onSubmit={submitHandler}>
+                    <Form onSubmit={(e) => { e.preventDefault(); setTaskText(""); }}>
                         <Row className="align-items-center">
                             <Col xs={9}>
                                 <Form.Control
                                     type="text"
-                                    value={text}
-                                    onChange={changeText}
+                                    value={taskText}
+                                    onChange={(e) => setTaskText(e.target.value)}
                                     placeholder="Add a new task"
                                 />
                             </Col>
@@ -65,6 +144,7 @@ function Todo() {
 
                     <ListGroup className="mt-3">
                     {tasks.map((task) => (
+                        <>
                         <ListGroup.Item key={task.id} className="todo-item d-flex justify-content-between align-items-center">
                             <InputGroup className="flex-grow-1">
                                 <InputGroup.Checkbox
@@ -77,10 +157,77 @@ function Todo() {
                                     </div>
                                 </div>
                             </InputGroup>
+                            <div className="button-group">
                             <div className="task-actions">
                                 <Button variant="danger" size="sm" onClick={() => removeTask(task.id)}>Remove</Button>
                             </div>
+                            <div className="task-actions">
+                            <Button 
+                                    size="sm"
+                                    onClick={() => toggleComment(task.id)}
+                                    aria-controls={`comment-collapse-${task.id}`}
+                                    aria-expanded={openComments[task.id]}
+                                >
+                                    comment
+                            </Button>
+                            </div>
+                            <div className="task-actions">
+                                <Button
+                                    size="sm"
+                                    variant='info'
+                                    onClick={() => toggleSubtaskVisibility(task.id)}
+                                    aria-controls={`subtask-collapse-${task.id}`}
+                                    aria-expanded={openSubtasks[task.id]}
+                                >
+                                    Subtask
+                                </Button>
+                            </div>
+                            </div>
                         </ListGroup.Item>
+                        <Collapse in={openComments[task.id]}>
+                            <div id={`comment-collapse-${task.id}`} className="todo-comment">
+                                <Form.Control
+                                    as="textarea"
+                                    value={task.comment || ''}
+                                    onChange={(e) => handleCommentChange(task.id, e.target.value)}
+                                    placeholder="Add a comment"
+                                />
+                            </div>
+                        </Collapse>
+                        <Collapse in={openSubtasks[task.id]}>
+                                    <div id={`subtask-collapse-${task.id}`}>
+                                    <ListGroup>
+                                    {task.subtasks && task.subtasks.map((subtask, index) => (
+                                        <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center">
+                                            <InputGroup className="flex-grow-1">
+                                                <InputGroup.Checkbox
+                                                    checked={subtask.completed}
+                                                    onChange={() => toggleSubtaskCompletion(task.id, index)}
+                                                />
+                                                <div className="task-text-container">
+                                                    <div className="task-text" style={{ textDecoration: subtask.completed ? 'line-through' : 'none' }}>
+                                                        {subtask.text}
+                                                    </div>
+                                                </div>
+                                            </InputGroup>
+                                            <Button variant="danger" size="sm" onClick={() => deleteSubtask(task.id, index)}>Delete</Button>
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                                        <Form onSubmit={(e) => { e.preventDefault(); addSubtask(task.id, subtaskText); }}>
+                                            <InputGroup>
+                                                <Form.Control
+                                                    type="text"
+                                                    value={subtaskText}
+                                                    onChange={(e) => setSubtaskText(e.target.value)}
+                                                    placeholder="Add a new subtask"
+                                                />
+                                                <Button type="submit" variant="primary" size="sm">Add</Button>
+                                            </InputGroup>
+                                        </Form>
+                                    </div>
+                                </Collapse>
+                        </>
                     ))}
             </ListGroup>
                 </Col>
